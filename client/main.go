cc7feea04fb3fb1c87092c5111654c10314b0c2f
juampicarosi <juampicarosi@gmail.com>
2025-03-23 14:20:42 -0300

    sleep 100ms before closing

--- client/main.go
+++ client/main.go
@@ -145,6 +145,6 @@ func main() {
 	client.SendBatches()
 
 	client.SendResultsQuery()
-	time.Sleep(10 * time.Millisecond)
+	time.Sleep(100 * time.Millisecond)
 	defer client.Cleanup(syscall.SIGTERM)
 }
